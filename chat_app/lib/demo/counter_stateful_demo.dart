6853c202bb90541da051edd889df5838bae8de74
Houdini <houdini.programmer@gmail.com>
2022-11-20 17:03:25 -0500

    Completed 4_2b

--- chat_app/lib/demo/counter_stateful_demo.dart
+++ chat_app/lib/demo/counter_stateful_demo.dart
@@ -1,2 +1,38 @@
-
 //TODO: Recreate the counter app
+
+import 'package:flutter/material.dart';
+
+class CounterStateful extends StatefulWidget {
+  const CounterStateful({Key? key}) : super(key: key);
+
+  @override
+  State<CounterStateful> createState() => _CounterStatefulState();
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: (Icon(Icons.add)),
+        onPressed: () {
+          increment();
+        },
+      ),
+      body: Center(
+        child: Text(
+          "$counter",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
