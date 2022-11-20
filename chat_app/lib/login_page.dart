9559bcf9dd4c73216291f85de6e0ba7e4355f74f
Houdini <houdini.programmer@gmail.com>
2022-11-20 03:08:23 -0500

    Added chat_app and loginWidget

---
+++ chat_app/lib/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      body: Text("Hello"),
+    );
+  }
+}
