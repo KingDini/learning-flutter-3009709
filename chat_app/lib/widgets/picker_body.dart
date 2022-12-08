685b38cefa9e19bce49920f4d867330c5847cb45
Houdini <houdini.programmer@gmail.com>
2022-12-08 17:03:41 -0500

    Completed

--- chat_app/lib/widgets/picker_body.dart
+++ chat_app/lib/widgets/picker_body.dart
@@ -3,9 +3,9 @@ import 'package:chat_app/repo/image_repository.dart';
 import 'package:flutter/material.dart';
 
 class NetworkImagePickerBody extends StatelessWidget {
-  NetworkImagePickerBody({
-    Key? key,
-  }) : super(key: key);
+  final Function(String) onImageSelected;
+  NetworkImagePickerBody({Key? key, required this.onImageSelected})
+      : super(key: key);
 
   final ImageRepository _imageRepo = ImageRepository();
 
@@ -20,7 +20,11 @@ class NetworkImagePickerBody extends StatelessWidget {
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 //TODO: Add a Gesture Detector to register clicks on each image
-                return Image.network(snapshot.data![index].urlSmallSize);
+                return GestureDetector(
+                    onTap: () {
+                      onImageSelected(snapshot.data![index].urlSmallSize);
+                    },
+                    child: Image.network(snapshot.data![index].urlSmallSize));
               },
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                   crossAxisSpacing: 2,
