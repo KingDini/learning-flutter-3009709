3f1c231175037c09c83553ab70b065efce082827
Houdini <houdini.programmer@gmail.com>
2022-12-09 12:13:41 -0500

    Completed

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -31,6 +31,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
@@ -55,11 +56,17 @@ class LoginPage extends StatelessWidget {
                     fontSize: 20,
                     color: Colors.blueGrey),
               ),
-              //TODO: Make rounded borders
-              Image.asset(
-                'assets/illustration.png',
+              verticalSpacing(24),
+              Container(
                 height: 200,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                        fit: BoxFit.fitWidth,
+                        image: AssetImage('assets/illustration.png'))),
               ),
+              verticalSpacing(24),
               Form(
                 key: _formkey,
                 child: Column(
